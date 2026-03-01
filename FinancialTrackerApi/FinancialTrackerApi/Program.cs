f093b50252696517259f9490512da04001acc337
aymen mejri <noise_team@hotmail.com>
2026-02-28 19:01:31 -0500

    Neonpostgres Entity framework config

--- FinancialTrackerApi/FinancialTrackerApi/Program.cs
+++ FinancialTrackerApi/FinancialTrackerApi/Program.cs
@@ -1,8 +1,25 @@
+using FinancialTrackerApi.db;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+// --- 1. Read configuration and build connection string ---
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+builder.Services.AddDbContext<FinancialTrackerDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
---
+++ FinancialTrackerApi/FinancialTrackerApi/db/FinancialTrackerDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
+
+namespace FinancialTrackerApi.db
+{
+    public class FinancialTrackerDbContext : DbContext
+    {
+        public FinancialTrackerDbContext(DbContextOptions<FinancialTrackerDbContext> options)
+    : base(options)
+        {
+        }
+        public DbSet<Category> Categories => Set<Category>();
+
+        #region Required
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>()
+                .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Category>()
+                .ToTable("Categories");
+        }
+        #endregion
+
+    }
+}
+
