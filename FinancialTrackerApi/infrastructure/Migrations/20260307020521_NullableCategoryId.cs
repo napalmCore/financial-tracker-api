2db9127f6da339b4fa68a8cde0fd15656e500a1e
aymen mejri <noise_team@hotmail.com>
2026-03-06 21:16:27 -0500

    Changed CategoryId Type to Nullable

---
+++ FinancialTrackerApi/infrastructure/Migrations/20260307020521_NullableCategoryId.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinancialTrackerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableCategoryId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Categories_CategoryId",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Transactions",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Categories_CategoryId",
+                table: "Transactions",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_Categories_CategoryId",
+                table: "Transactions");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Transactions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_Categories_CategoryId",
+                table: "Transactions",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
